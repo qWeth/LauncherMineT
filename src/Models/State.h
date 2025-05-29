1caeb0aba2253db3f30557620ece646b7a468dc7
mikle <mishaiordanskiy@gmail.com>
2025-05-30 02:54:50 +0400

    bug fix

--- src/Models/State.h
+++ src/Models/State.h
@@ -1,29 +1,27 @@
 #pragma once
 
-#include "GameProfile.h"
-#include "Account.h"
 #include <AUI/Common/AProperty.h>
 
 struct State {
-    struct Profiles {
-
-        void notify() {
-            list.notify();
-            selected.notify();
-        }
-    } profile;
-
-    struct Accounts {
-        //        AProperty<AVector<_<Account>>> list;
-        //        AProperty<_<Account>> selected;
-        _<Account> current = _new<Account>();
-    } accounts;
-
-    /**
-     * @brief Used to track updates in launcher_profiles.json.
-     * @details
-     * When using Forge or OptiFine installer, it populates launcher_profiles.json with new profiles. MainWindow
-     * compares for that every time it acquires focus.
-     */
-    ASet<AUuid> profilesUuidsSnapshot;
+    // struct Profiles {
+    //
+    //     void notify() {
+    //         list.notify();
+    //         selected.notify();
+    //     }
+    // } profile;
+    //
+    // struct Accounts {
+    //     //        AProperty<AVector<_<Account>>> list;
+    //     //        AProperty<_<Account>> selected;
+    //     _<Account> current = _new<Account>();
+    // } accounts;
+    //
+    // /**
+    //  * @brief Used to track updates in launcher_profiles.json.
+    //  * @details
+    //  * When using Forge or OptiFine installer, it populates launcher_profiles.json with new profiles. MainWindow
+    //  * compares for that every time it acquires focus.
+    //  */
+    // ASet<AUuid> profilesUuidsSnapshot;
 };
